d0c459dbb70b0d6a2ce48bc0464028dadaae287f
Younes JAAIDI <yjaaidi@gmail.com>
2018-05-25 09:30:34 +0200

    Merge all chapters in one PDF file.

--- index.mjs
+++ index.mjs
@@ -1,35 +1,99 @@
+/**
+ *
+ * (c) 2013-2018 Wishtack
+ * https://wishtack.io
+ *
+ */
+
+import addZero from 'add-zero';
+import commander from 'commander';
+import easyPdfMerge from 'easy-pdf-merge';
+import fs from 'fs';
+import path from 'path';
 import puppeteer from 'puppeteer';
+import rimraf from 'rimraf';
+import util from 'util';
 
 export class GitbookPrinter {
 
-    async printGitBook({summaryPath, url}) {
+    constructor({baseUrl, outPath, summaryPath}) {
+        this._baseUrl = baseUrl;
+        this._outPath = outPath;
+        this._partialsPath = path.join(this._outPath, 'partials');
+        this._summaryPath = summaryPath;
+    }
 
+    async savePdf() {
+
+        /* Clean up target. */
+        await util.promisify(rimraf)(this._outPath);
+
+        /* Create target directories. */
+        fs.mkdirSync(this._outPath);
+        fs.mkdirSync(this._partialsPath);
+
+        /* Download partial files. */
+        const pdfFilePathList = await this._downloadChapterList();
+
+        /* Merge files. */
+        await util.promisify(easyPdfMerge)(pdfFilePathList, path.join(this._outPath, 'gitbook.pdf'));
+
+    }
+
+    async _downloadChapterList() {
+
+        /* Open browser. */
         const browser = await puppeteer.launch();
 
         const page = await browser.newPage();
 
-        const pathList = await this._summaryPathToPathList({summaryPath});
+        /* Get chapters list from summary. */
+        const chapterPathList = await this._getChapterPathList();
 
-        for (const [index, path] of pathList.entries()) {
+        let pdfFilePathList = [];
 
-            const pageUrl = `${url}/${path}`;
-            const pdfPath = `out/${100 + index}-${path.replace(/\//g, '-') || 'welcome'}.pdf`;
+        for (const [index, chapterPath] of chapterPathList.entries()) {
+
+            const pageUrl = `${this._baseUrl}/${chapterPath}`;
+
+            const pdfFilePath = this._getChapterPdfFilePath({index, chapterPath});
 
             console.debug(`Printing: ${pageUrl}...`);
             await this._printPage({
                 page,
                 pageUrl,
-                pdfPath
+                pdfFilePath
             });
-            console.debug(`Created: ${pdfPath}.`);
+
+            /* Add file to list in order to merge all files later. */
+            pdfFilePathList = [...pdfFilePathList, pdfFilePath];
+
+            console.debug(`Created: ${pdfFilePath}.`);
 
         }
 
         await browser.close();
 
+        return pdfFilePathList;
+
     }
 
-    async _printPage({page, pageUrl, pdfPath}) {
+    _getChapterPdfFilePath({index, chapterPath}) {
+
+        const chapterIndexDigitCount = 3;
+
+        let chapterFileName = addZero(index, chapterIndexDigitCount);
+        if (chapterPath.length > 0) {
+            chapterFileName += `-${chapterPath.replace(/\//g, '-')}`;
+        }
+
+        chapterFileName += '.pdf';
+
+        return path.join(this._partialsPath, chapterFileName);
+
+    }
+
+    async _printPage({page, pageUrl, pdfFilePath}) {
 
         await page.goto(pageUrl, {
             waitUntil: 'networkidle2'
@@ -43,18 +107,18 @@ export class GitbookPrinter {
         });
 
         await page.pdf({
-            path: pdfPath,
+            path: pdfFilePath,
             format: 'A4',
             landscape: false
         });
 
     }
 
-    async _summaryPathToPathList({summaryPath}) {
+    async _getChapterPathList() {
 
-        const { default: fs } = await import('fs');
+        const {default: fs} = await import('fs');
 
-        const summary = fs.readFileSync(summaryPath, 'utf-8');
+        const summary = fs.readFileSync(this._summaryPath, 'utf-8');
 
         return summary
             .split('\n')
@@ -67,8 +131,18 @@ export class GitbookPrinter {
     }
 }
 
-new GitbookPrinter()
-    .printGitBook({
-        summaryPath: '/Users/r/dev/wishtack/guide-angular/SUMMARY.md',
-        url: 'https://guide-angular.wishtack.io'
-    });
+commander
+    .option('-b, --base-url <baseUrl>', 'Gitbook Base URL')
+    .option('-s, --summary-path <summaryPath>', 'File Path to SUMMARY.md')
+    .parse(process.argv);
+
+if (commander.baseUrl == null || commander.summaryPath == null) {
+    commander.help();
+}
+
+new GitbookPrinter({
+    baseUrl: commander.baseUrl,
+    summaryPath: commander.summaryPath,
+    outPath: 'out'
+})
+    .savePdf();
